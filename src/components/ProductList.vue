a2b5218d40285e389e80b5d52442c73de2c39a7d
dylan <dylanglandry@gmail.com>
2023-08-29 16:35:55 -0400

    remove some vite html and css boiler

---
+++ src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useProductStore } from '@/stores/product';
+
+const { products } = useProductStore();
+</script>
+
+<template>
+    <div :style="{ display: 'flex', justifyContent: 'space-between' }">
+        <div v-for="product in products" :key="product.id">
+            <img :src="product.image" :style="{
+                display: 'block',
+                width: '200px',
+                height: '200px'
+            }
+                ">
+            <div>${{ product.price }}</div>
+            <div>{{ product.name }}</div>
+            <button :style="{ display: 'block' }">Add to Cart</button>
+            <button :style="{ display: 'block' }">Add to Wishlist</button>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,8 @@
 <script setup lang="ts">
-import { useProductStore } from '@/stores/product';
-
-const { products } = useProductStore();
+import ProductList from '@/components/ProductList.vue';
 
 </script>
 
 <template>
-  <main>
-    <div :style="{ display: 'flex', justifyContent: 'space-between' }">
-      <div v-for="  product   in   products  " :key="product.id">
-        <img :src="product.image" :style="{
-          display: 'block',
-          width: '200px',
-          height: '200px'
-        }
-          ">
-        <div>${{ product.price }}</div>
-        <div>{{ product.name }}</div>
-        <button :style="{ display: 'block' }">Add to Cart</button>
-        <button :style="{ display: 'block' }">Add to Wishlist</button>
-      </div>
-    </div>
-  </main>
+  <ProductList />
 </template>
