896ce18a87d17f47e508d725d6cdbe39d9dd6c76
dylan <dylanglandry@gmail.com>
2023-09-04 16:48:57 -0400

    add placeholder navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="m-4 max-w-[800px]">
+    <Navbar/>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+    <div class="flex items-end mt-2 mb-4">
+        <div class="text-xl grow-[2]">
+            <RouterLink :to="{ name: 'home' }">Vue Shoppe</RouterLink>
+        </div>
+        <div class="grow">
+            <RouterLink :to="{ name: 'home' }">Products</RouterLink>
+        </div>
+        <div class="grow">
+            <RouterLink :to="{ name: 'home' }">Cart</RouterLink>
+        </div>
+        <div class="grow">
+            <RouterLink :to="{ name: 'home' }">Wishlist</RouterLink>
+        </div>
+    </div>
+</template>
